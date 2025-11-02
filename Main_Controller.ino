4042148dde1053939f839082442ba5b9d5bfbfa4
navinkumar1288 <89594000+navinkumar1288@users.noreply.github.com>
2025-11-02 23:02:35 +0530

    Add files via upload

--- Main_Controller.ino
+++ Main_Controller.ino
@@ -10,26 +10,26 @@
 #include "ble_mod.h"
 
 void setup() {
-  Serial.begin(115200);
-  delay(200);
-  initStorage();
-  prefs.begin("irrig", false);
+  Serial.begin(115200); delay(200);
+  initStorage(); prefs.begin("irrig", false);
   displayInitHeltec();
   loadSystemConfig();
+  if (!LittleFS.exists("/schedules")) LittleFS.mkdir("/schedules");
   loadAllSchedulesFromFS();
 
-  // RTC init
+  // RTC TwoWire init
   WireRTC.begin(RTC_SDA, RTC_SCL, 100000);
+  delay(20);
   rtcAvailable = rtc.begin(&WireRTC);
   if (rtcAvailable) {
-    Serial.println("RTC detected");
+    Serial.println("RTC detected on WireRTC");
     if (rtc.lostPower()) rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  } else Serial.println("RTC not detected");
+  } else Serial.println("RTC not detected on WireRTC bus");
 
   radioInit();
   modemInit();
   modemConfigureAndConnectMQTT();
-  // initBLE();
+  //initBLE();
   lastStatusPublish = millis();
   Serial.println("Setup complete");
 }
@@ -39,14 +39,13 @@ void loop() {
   Radio.IrqProcess();
   handleLoRaIncoming();
 
-  String iq;
-  if (dequeueIncoming(iq)) {
-    Serial.println("Processing queued incoming: " + iq);
-    processIncomingScheduleString(iq);
-  }
+  // process one incoming queued message
+  String iq; if (dequeueIncoming(iq)) { Serial.println("Processing queued incoming: " + iq); processIncomingScheduleString(iq); }
 
   runScheduleLoop();
-  periodicTasks();
+
+  periodicTasks(); // from utils/scheduler (status publish, scheduler check, rtc drift)
+
   displayLoop();
   delay(20);
 }
