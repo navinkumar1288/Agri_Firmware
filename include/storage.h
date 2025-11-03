b4516b48ed8047eccda4377e6c6245e00bbc95c0
navinkumar1288 <89594000+navinkumar1288@users.noreply.github.com>
2025-11-03 15:04:59 +0530

    Update storage.h

--- include/storage.h
+++ include/storage.h
@@ -8,7 +8,6 @@ bool initStorage();
 bool saveStringFile(const String &path, const String &content);
 String loadStringFile(const String &path);
 
-extern std::vector<Schedule> schedules;
 
 bool saveScheduleFile(const Schedule &s);
 Schedule scheduleFromJsonString(const String &json);
