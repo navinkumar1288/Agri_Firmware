205601b628f98a2337220e137c09778e697f0617
navinkumar1288 <89594000+navinkumar1288@users.noreply.github.com>
2025-11-03 10:06:49 +0530

    Update storage.h

--- include/storage.h
+++ include/storage.h
@@ -1,11 +1,5 @@
 #pragma once
-
-struct SeqStep { int node_id; uint32_t duration_ms; };
-struct Schedule {
-  String id; char rec; time_t start_epoch; String timeStr; uint8_t weekday_mask;
-  std::vector<SeqStep> seq; uint32_t pump_on_before_ms; uint32_t pump_off_after_ms;
-  bool enabled; time_t next_run_epoch; uint32_t ts;
-};
+#include 'config.h'
 
 bool initStorage();
 bool saveStringFile(const String &path, const String &content);
