7aa55a47a8510b68a6b69af26156175f60ca355c
navinkumar1288 <89594000+navinkumar1288@users.noreply.github.com>
2025-11-03 15:03:48 +0530

    Update storage.h

--- include/storage.h
+++ include/storage.h
@@ -2,12 +2,7 @@
 #include "utils.h"
 #include <ArduinoJson.h>
 
-struct SeqStep { int node_id; uint32_t duration_ms; };
-struct Schedule {
-  String id; char rec; time_t start_epoch; String timeStr; uint8_t weekday_mask;
-  std::vector<SeqStep> seq; uint32_t pump_on_before_ms; uint32_t pump_off_after_ms;
-  bool enabled; time_t next_run_epoch; uint32_t ts;
-};
+
 
 bool initStorage();
 bool saveStringFile(const String &path, const String &content);
